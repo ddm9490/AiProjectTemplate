22f924da85e5cc26e3eb27471229613f4881e381
ddm9490 <ddm9490@gmail.com>
2025-05-15 21:11:29 +0900

    abstraction

---
+++ src/my_template/abc_interfaces/data_interfaces.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+
+
+class BaseDataLoader(ABC):
+
+	def __init__(self,config):
+		self.set_config(config)
+		self.load_data()
+
+	@abstractmethod
+	def load_data(self):
+		pass
+
+	@abstractmethod
+	def set_config(self):
+		pass
+
+
+class BaseDataset(ABC):
+
+	@abstractmethod
+	def __init__(self):
+		pass
+
+	@abstractmethod
+	def __len__(self):
+		pass
+
+	@abstractmethod
+	def __getitem__(self):
+		pass
+
+class BaseDataProcessor(ABC):
+
+	def __init__(self):
+		pass
+
+	@abstractmethod
+	def process_data(self,data):
+		pass
