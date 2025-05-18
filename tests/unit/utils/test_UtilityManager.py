3bae2eeb3b46dff89fdc0bb17344ab622e401ffe
ddm9490 <58236851+ddm9490@users.noreply.github.com>
2025-05-18 12:34:10 +0900

    test_UtilityManager.py 업데이트

--- tests/unit/utils/test_UtilityManager.py
+++ tests/unit/utils/test_UtilityManager.py
@@ -1,2 +1,16 @@
 from ....src.my_template.utils import UtilityManager
 import pytest
+
+@pytest.fixture
+def my_test_instance() -> UtilityManager:
+  instance = UtilityManager()
+  return instance
+
+def test_set_seed(my_test_instance) -> None:
+  my_test_instance.set_seed(42)
+
+def test_root_path(my_test_instance) -> None:
+  print(my_test_instance.root_path)
+
+def test_config_path(my_test_instance) -> None:
+  print(my_test_instance.config_path)
