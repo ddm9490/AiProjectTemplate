bee09fb788927ffd48d0a89bc331af9c5ce9a007
ddm9490 <58236851+ddm9490@users.noreply.github.com>
2025-05-19 01:02:49 +0900

    test_UtilityManager.py 업데이트

--- tests/unit/utils/test_UtilityManager.py
+++ tests/unit/utils/test_UtilityManager.py
@@ -11,9 +11,3 @@ def test_root_path(test_utility_manager) -> None:
 def test_config_path(test_utility_manager) -> None:
     print(test_utility_manager.config_path)
 
-
-def test_print_test(test_utility_manager) -> None:
-    print("This is a test message.")
-    print("This is a test message.", "INFO")
-    print("This is a test message.", "WARNING")
-    print("This is a test message.", "ERROR")
