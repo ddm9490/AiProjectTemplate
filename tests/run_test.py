a17e5b3976ea592186dca1cbb111da21618db2ca
ddm9490 <58236851+ddm9490@users.noreply.github.com>
2025-05-18 13:45:05 +0900

    pytest

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,5 +1,13 @@
 
 import pytest
 
-if __name__ == "__main__":
+def run_test() -> None:
     pytest.main()
+
+def run_unit_test() -> None:
+    pytest.main(["tests/unit"])
+
+def run_integration_test() -> None:
+    pytest.main(["tests/integration"])
+if __name__ == "__main__":
+    run_test()
